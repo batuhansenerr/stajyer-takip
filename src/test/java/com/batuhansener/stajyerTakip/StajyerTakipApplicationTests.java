aca40cb2cb114768cfb348111894cb8c9fc52abe
abatuhan06 <abatuhan0666@gmail.com>
2024-03-27 10:21:00 +0300

    test added

---
+++ src/test/java/com/batuhansener/stajyerTakip/StajyerTakipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuhansener.stajyerTakip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajyerTakipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
