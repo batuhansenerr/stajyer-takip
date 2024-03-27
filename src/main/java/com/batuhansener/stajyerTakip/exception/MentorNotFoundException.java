0df2eea366502beeae0b6eafd8e363d0743e78ab
abatuhan06 <abatuhan0666@gmail.com>
2024-03-27 10:19:23 +0300

    exceptions added

---
+++ src/main/java/com/batuhansener/stajyerTakip/exception/MentorNotFoundException.java
@@ -0,0 +1,8 @@
+package com.batuhansener.stajyerTakip.exception;
+
+public class MentorNotFoundException extends RuntimeException{
+
+    public MentorNotFoundException(String message){
+        super(message);
+    }
+}
