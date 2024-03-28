08f98961d8474c662d7bf62dcd92028ddbfd1692
abatuhan06 <abatuhan0666@gmail.com>
2024-03-28 23:46:20 +0300

    repositories refactored

--- src/main/java/com/batuhansener/stajyerTakip/repository/MentorRepository.java
+++ src/main/java/com/batuhansener/stajyerTakip/repository/MentorRepository.java
@@ -2,10 +2,12 @@ package com.batuhansener.stajyerTakip.repository;
 
 import com.batuhansener.stajyerTakip.model.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MentorRepository  extends JpaRepository<Mentor, String> {
+    Mentor findByUserId(String user_id);
 }
--- src/main/java/com/batuhansener/stajyerTakip/repository/UserRepository.java
+++ src/main/java/com/batuhansener/stajyerTakip/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.batuhansener.stajyerTakip.repository;
 
 import com.batuhansener.stajyerTakip.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
     Optional<User> findByUsername(String username);
 }
